61869136091eaa70971c4b9c32894e7ff34096b7
Mojatto <daichiandroid1997@gmail.com>
2021-02-18 18:43:32 +0900

    プロジェクトを作成，サンプルコードを追加．

---
+++ Project1/Main.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+using namespace std;
+
+void DebugOutputFormatString(const char* format, ...) {
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+#endif
+}
+
+#ifdef _DEBUG
+int main() {
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#endif
+	DebugOutputFormatString("TEST MESSAGE.");
+	getchar();
+	return 0;
+}
